cb91ce6842f15a6acbe8ab73bea34335d3f415c6
bc990424 <behknqux1234j@gmail.com>
2024-05-31 10:53:09 +0900

    dd

--- ss.py
+++ ss.py
@@ -1,33 +1,5 @@
 import numpy as np
+import socket
+from _thread import *
 
-
-
-a=np.zeros((16,16))
-
-class omok:
-    def __init__(self):
-        self.dx = [-1, 1, -1, 1, 0, 0, 1, -1]
-        self.dy = [0, 0, -1, 1, -1, 1, -1, 1]
-
-    def move(self,x,y,m):
-        if a[x,y] == 0:
-            for i in self.dx:
-                for n in self.dy:
-                    try:
-                        for z in range(1,6,1):
-                            if not a[x+i*z,y+z*n] == m:
-                                end= False
-                                break
-                        if end and not a[x+6*i,y+6*n] == m:
-                            print("승")
-
-                    except:
-                        pass
-
-
-
-
-
-
-w= omok()
-w.move(2,3,4)
+socket.socket
