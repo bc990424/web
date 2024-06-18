46b23b77989c443d7ad3819410745b81050ec72c
bc990424 <behknqux1234j@gmail.com>
2024-06-18 09:59:05 +0900

    gg

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def posting(request, pk):
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
     post = Post.objects.get(pk=pk)
     # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
-    return render(request, 'main/posting.html', {'post':post , 'menu' : menu.result(today.strftime("%Y%m%d")})
+    return render(request, 'main/posting.html', {'post':post , 'menu' : menu.result(today.strftime("%Y%m%d"))})
 
 def new_post(request):
     if request.method == 'POST':
